15b9a85ac58a9f61002cfe9bb527b2a63c8f759a
Alameer Ashraf Hassan <alamiir.ashraf@gmail.com>
2017-04-08 17:46:44 +0200

    Initilaztion Workflow

---
+++ AdminstrationSysytem-v1/AdminstrationSysytem-v1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdminstrationSysytem_v1.Startup))]
+namespace AdminstrationSysytem_v1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
